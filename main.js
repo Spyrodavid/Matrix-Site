f8e59fefaaaae0ca5ac7f0e0bd774e3aace8b35e
David Saifer <spyrotheultimate@gmail.com>
2023-02-14 00:36:36 -0800

    working non-live

--- main.js
+++ main.js
@@ -1,41 +1,29 @@
 // get context for each input
-const input_canvas = document.getElementById("input_canvas");
-const input_ctx = input_canvas.getContext("2d");
-
-const output_canvas = document.getElementById("output_canvas");
-const output_ctx = output_canvas.getContext("2d");
+const canvas = document.getElementById("input_canvas");
+const input_ctx = canvas.getContext("2d");
 
 const matrix_section = document.getElementById("matrix_section");
 
 const validity = document.getElementById("validity");
 
-input_canvas.height = parseInt(input_canvas.width * .7)
-output_canvas.height = parseInt(output_canvas.width * .7)
+canvas.height = parseInt(canvas.width * .7)
 
-
-// get dimension of each canvas
-out_width = output_canvas.width
-out_height = output_canvas.height
-
-in_width = input_canvas.width
-in_height = input_canvas.height
+// get dimension of canvas
+in_width = canvas.width
+in_height = canvas.height
 
 input_ctx.translate(in_width / 2, in_height / 2)
-output_ctx.translate(out_width / 2, out_height / 2)
 
 input_ctx.scale(1, -1)
-output_ctx.scale(1, -1)
 
 
 input_points = []
-output_points = []
 input_points_radius = 5
 
-input_canvas.addEventListener("click", (e) => {
+canvas.addEventListener("click", (e) => {
     input_points.push(math.matrix([
         e.offsetX - in_width / 2, -(e.offsetY - in_height / 2)
     ]))
-    output_points = [...input_points]
 });
 
 var program_start = Date.now()
@@ -43,15 +31,13 @@ var program_start = Date.now()
 var t1 = Date.now()
 var t2 = Date.now()
 
+newMatrix([])
 
 function MainLoop() {
 
 
     input_ctx.fillStyle = "white";
     input_ctx.fillRect(-in_width / 2, -in_height / 2, in_width, in_height)
-
-    output_ctx.fillStyle = "white";
-    output_ctx.fillRect(-out_width / 2, -out_height / 2, out_width, out_height)
 
     // draw grid lines on Input
     input_ctx.beginPath();
@@ -62,92 +48,54 @@ function MainLoop() {
     input_ctx.lineTo(-in_width / 2, 0);
     input_ctx.stroke();
 
+    
 
-    // draw grid lines on Output
-    output_ctx.beginPath();
-    output_ctx.moveTo(0, in_height / 2);
-    output_ctx.lineTo(0, -in_height / 2);
-
-    output_ctx.moveTo(in_width / 2, 0);
-    output_ctx.lineTo(-in_width / 2, 0);
-    output_ctx.stroke();
-
-
-    validity.innerHTML = "All Matrices Valid!"
+    validity.innerHTML = "Matrix is Valid!"
     validity.style.color = "black"
-    //matrix_settings_node.getElementsByClassName("matrix_apply_button")[0].disabled = false
 
-    matrices_to_multiply = []
+    document.getElementsByClassName("matrix_apply_button")[0].disabled = false
+
 
     matrix_nodes = matrix_section.getElementsByClassName("matrix")
-    for (let i = 0; i < matrix_nodes.length; i++) {
-        var matrix_node = matrix_nodes[i]
-        matrix_inputs = matrix_node.children
 
-        var is_valid_flag = true
-        var is_live_matrix_flag = true
-        var is_number_flag = true
+    var matrix_node = matrix_nodes[0]
+    
+        
+    matrix_inputs = matrix_node.children
 
-
-        matrix_values = Array.from(matrix_inputs).map((el) => {return el.value})
-
-        // check is live matrix
-        if (!matrix_values.some((el) => 
-                            {return el.includes("th")})) 
-        {
-            is_live_matrix_flag = false
-        }
+    var is_valid_flag = true
+    var is_number_flag = true
 
 
-        // replace time elapsed
-        if (is_live_matrix_flag) {
-            theta = (Date.now() - program_start) / 1000
-            matrix_values = matrix_values.map((el) => 
-                                {return el.replaceAll("th", "(" + String(theta) + ")")})
-        }
+    matrix_values = Array.from(matrix_inputs).map((el) => {return el.value})
 
-        // make sure expression is fully formed
-        try {
-            var matrix_values = matrix_values.map((el) => 
-                            {return el === "" ? 0 : math.evaluate(el)})
-            is_valid_flag = true
-        } catch (err) {
-            is_valid_flag = false
-        }
 
-        // make sure expression evaluates to number
-        if (is_valid_flag) {
+    // make sure expression is fully formed
+    try {
+        var matrix_values = matrix_values.map((el) => 
+                        {return el === "" ? 0 : math.evaluate(el)})
+        is_valid_flag = true
+    } catch (err) {
+        is_valid_flag = false
+    }
+
+    // make sure expression evaluates to number
+    if (is_valid_flag) {
+        
+        for (const value of matrix_values) { 
             
-            for (const value of matrix_values) { 
-                
-                if (isNaN(Number(value))) {
-                    is_number_flag = false
-                }
+            if (isNaN(Number(value))) {
+                is_number_flag = false
             }
         }
+    }
 
 
-        if (!(is_valid_flag && is_number_flag)) {
-            validity.innerHTML = "Invalid Value in a Matrix!"
-            validity.style.color = "red"
-            //matrix_settings_node.getElementsByClassName("matrix_apply_button")[0].disabled = true
-            break
-        }
-
-        // allready exited if invalid matrix
-        if (is_live_matrix_flag) {
-            new_matrix = math.matrix([
-                [
-                    matrix_values[0],
-                    matrix_values[1]
-                ],
-                [
-                    matrix_values[2],
-                    matrix_values[3]
-                ]
-            ])
-            matrices_to_multiply.push(new_matrix)
-        }
+    if (!(is_valid_flag && is_number_flag)) {
+        validity.innerHTML = "Invalid Value in Matrix!"
+        validity.style.color = "red"
+        document.getElementsByClassName("matrix_apply_button")[0].disabled = true
+        
     }
 
     // input draw
@@ -162,28 +110,9 @@ function MainLoop() {
         input_ctx.stroke();
     }
 
-    // output draw
-    for (let point_index = 0; point_index < output_points.length; point_index++) {
-        var current_point = output_points[point_index];
-
-        // go through each matrix
-        for (let matrix_index = 0; matrix_index < matrices_to_multiply.length; matrix_index++) {
-            const current_matrix = matrices_to_multiply[matrix_index];
-
-            current_point = math.multiply(current_point, current_matrix)
-        }
-
-        // draw point
-        output_ctx.strokeStyle = `black`
-        output_ctx.beginPath();
-        output_ctx.arc(current_point.get([0]), current_point.get([1]), input_points_radius, 0, 2 * Math.PI);
-        output_ctx.arc(current_point.get([0]), current_point.get([1]), input_points_radius, 0, 2 * Math.PI);
-        output_ctx.stroke();
-        
-    }
-
-
     requestAnimationFrame(MainLoop)
+
+    
 
 }
 requestAnimationFrame(MainLoop)
@@ -228,15 +157,6 @@ function newMatrix(default_inputs) {
     matrix_buttons.classList.add("matrix_buttons")
     matrix_and_settings.appendChild(matrix_buttons)
 
-    
-    matrix_delete_button = document.createElement("button")
-    matrix_delete_button.innerHTML = "❌"
-    matrix_delete_button.classList.add("matrix_delete_button", "matrix_button")
-    matrix_delete_button.addEventListener("click", () => {
-        matrix_and_settings.remove()
-    })
-    matrix_buttons.appendChild(matrix_delete_button)
-
 
     matrix_apply_button = document.createElement("button")
     matrix_apply_button.innerHTML = "Apply Matrix"
@@ -252,24 +172,11 @@ function matrix_apply(matrix_node) {
         matrix_inputs = matrix_node.children
 
         var is_valid_flag = true
-        var is_live_matrix_flag = true
         var is_number_flag = true
 
-        // check is live matrix
-        if (!Array.from(matrix_inputs).some((el) => 
-                            {return el.value.includes("th")})) 
-        {
-            is_live_matrix_flag = false
-        }
+
 
         matrix_values = Array.from(matrix_inputs)
-
-        // replace time elapsed
-        if (is_live_matrix_flag) {
-            theta = (Date.now() - program_start) / 1000
-            matrix_values = matrix_inputs.map((el) => 
-                                {return el.value.replaceAll("th", "(" + String(theta) + ")")})
-        }
 
         // make sure expression is fully formed
         try {
@@ -292,7 +199,7 @@ function matrix_apply(matrix_node) {
         }
 
         if (!(is_valid_flag && is_number_flag)) {
-            validity.innerHTML = "Invalid Value in a Matrix!"
+            validity.innerHTML = "Invalid Value in Matrix!"
             validity.style.color = "red"
             //matrix_settings_node.getElementsByClassName("matrix_apply_button")[0].disabled = true
             return
@@ -309,7 +216,7 @@ function matrix_apply(matrix_node) {
                 matrix_values[3]
             ]
         ])
-        output_points = output_points.map((point) => {return math.multiply(point, new_matrix)})
+        input_points = input_points.map((point) => {return math.multiply(point, new_matrix)})
 }
 
 function clearPoints() {
